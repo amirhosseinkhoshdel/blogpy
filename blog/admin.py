f6aec15fcda45b9d9978cff0fd14cb6bbec3ab3c
imamirhossein <amirhosseinkhoshdel@gmail.com>
2020-10-23 12:17:29 +0330

    add list_display to admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(UserProfile)
-admin.site.register(Article)
-admin.site.register(Category)
+class UserProfileAdmin(admin.ModelAdmin):
+        list_display = ['user', 'avatar', 'description']
+
+admin.site.register(UserProfile, UserProfileAdmin)
+
+class ArticleAdmin(admin.ModelAdmin):
+        search_fields = ['title', 'content']
+        list_display = ['title', 'cover', 'created_at', 'updated_at', 'status']
+
+admin.site.register(Article, ArticleAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+        list_display = ['title', 'cover']
+
+admin.site.register(Category, CategoryAdmin)
